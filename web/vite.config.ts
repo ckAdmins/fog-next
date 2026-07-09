9f0bbfd06ab902be19f8c6986e64fd284a0b9bcf
Tamas Vince <nemvince@proton.me>
2026-07-09 17:40:03 +0200

    chore: update web

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,7 @@
-import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
@@ -10,7 +10,7 @@ export default defineConfig({
 		tanstackRouter({
 			target: "react",
 			autoCodeSplitting: true,
-			routeFileIgnorePrefix: "-",
+			routeFileIgnorePrefix: ".",
 		}),
 		react(),
 		tailwindcss(),
